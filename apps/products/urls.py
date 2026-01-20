ad1d72b7582e84ee803138fc3aa91854cb36b019
Daniel-IS04 <danieljosuecr8@gmail.com>
2026-01-20 15:12:56 -0500

    fix: rename package product view set

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from apps.products.views.views import ProductViewSet
+from apps.products.views.products import ProductViewSet
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet, basename='product')
